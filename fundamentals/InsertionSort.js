cba7419c5c0cf99043568293036e83550acdedd8
Chau Dinh <chaudvb95uc@gmail.com>
2019-06-14 20:08:53 +0700

    Adding sorting array in non-increasing order, and add jest test

--- fundamentals/InsertionSort.js
+++ fundamentals/InsertionSort.js
@@ -2,7 +2,7 @@
 
 // There are several sort method for us to deal with, in this section, we will focus on Insertion Sort, very basic and fundamentals. For more information, please check the book called "Introduction to Algorithms, 3rd edition" of MIT publishcation.
 
-const InsertionSort = arr => {
+module.exports.InsertionSort = arr => {
   if (arr.length === 0) {
     return `We don't need to sort this array`;
   }
@@ -21,6 +21,20 @@ const InsertionSort = arr => {
   return arr;
 };
 
-console.log(InsertionSort([1, 0, 0, -2, 3, 2918, -1299, 33, 0, 4]));
+module.exports.InsertionSortDecrease = arr => {
+  if (arr.length === 0) {
+    return `We don't need to sort this array`;
+  }
 
-module.exports = InsertionSort;
+  for (let j = 1; j < arr.length; j++) {
+    let key = arr[j];
+    let i = j - 1;
+    while (i >= 0 && arr[i] < key) {
+      arr[i + 1] = arr[i];
+      i = i - 1;
+    }
+    arr[i + 1] = key;
+  }
+
+  return arr;
+};
--- fundamentals/InsertionSort.test.js
+++ fundamentals/InsertionSort.test.js
@@ -1,17 +1,23 @@
 const InsertionSort = require("./InsertionSort");
 
 describe("Return sorted array", () => {
-  it("sorts an array of numbers", () => {
+  it("sorts an array of numbers in non-decreasing order", () => {
     let expected = [1, 2, 3];
-    let actual = InsertionSort([3, 2, 1]);
+    let actual = InsertionSort.InsertionSort([3, 2, 1]);
     expect(actual).toEqual(expected);
 
     expected = [-1, 0, 1, 2, 3];
-    actual = InsertionSort([3, 2, 1, 0, -1]);
+    actual = InsertionSort.InsertionSort([3, 2, 1, 0, -1]);
     expect(actual).toEqual(expected);
 
     expected = [-5, -1, 2, 3, 10, 20];
-    actual = InsertionSort([3, 2, -1, 20, -5, 10]);
+    actual = InsertionSort.InsertionSort([3, 2, -1, 20, -5, 10]);
+    expect(actual).toEqual(expected);
+  });
+
+  it("sorts an array of numbers in non-increasing order", () => {
+    let expected = [3, 2, 1];
+    let actual = InsertionSort.InsertionSortDecrease([1, 2, 3]);
     expect(actual).toEqual(expected);
   });
 });
